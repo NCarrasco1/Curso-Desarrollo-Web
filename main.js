5b9c4c92a939bd65caaae963245b73dc0876c113
Nicolas Carrasco <nicocarrascc@gmail.com>
2023-09-04 09:57:00 -0300

    3er preentrega

--- main.js
+++ main.js
@@ -1,69 +1,51 @@
 //3er preentrega 
+const contProductos = document.querySelector(`#cont-productos`);
+console.log(productos);
 
-  let usuario; 
-  let usuarioStorage = sessionStorage.getItem("usuario"); 
+const mostrarProductos = (d) => {
+  d.forEach(producto => {
+    const cardProducto = document.createElement(`article`);
+    cardProducto.setAttribute(`id`, `tarjeta-producto`); 
+    cardProducto.innerHTML =`
+        <img class = "prod-img" src ="${producto?.img}" alt="${producto?.nombre}" style="width: 150px"
+        <div class = "producto-description">
+          <h3 class = "producto-nombre">${producto?.nombre}</h3>
+          <h3 class = "producto-marca">${producto?.marca}</h3>
+          <h3 class = "producto-precio">$${producto?.precio}</h3>
+          <button id = '${producto.id}' class = "btn-compra"> COMPRAR</button>
+        </div> 
+        `; 
+    contProductos.appendChild(cardProducto); 
+  })
+  const btnComprar  = document.querySelectorAll('.btn-comprar'); 
+  btnComprar.forEach (el => {
+      el.addEventListener('click', (e) => {
+          agregarAlCarrito(e.target.id)
+      });  
+  })
+}
 
-  if (usuarioStorage) {
-    usuario = usuarioStorage;
-    alert(`Bienvenido/a nuevamente ${usuario}`);
-  } else {
-    usuario = prompt("Ingrese el usuario");
-    sessionStorage.setItem("usuario", usuario); 
-    alert(`Bienvenido/a por primera vez ${usuario}`); 
-  }; 
+const mostrarCarrito = () => {
+  console.log("Carrito de compras:");
+  carrito.forEach(producto => {
+    console.log(`- ${producto.nombre}`);
+  });
+}
+
+mostrarProductos(productos); 
+
+function agregarAlCarrito(id){
+  let prodEncontrado = productos.find (prod => prod.id === parseInt(id));
+  
+  carrito.push(prodEncontrado);
+  mostrarCarrito(); 
+}; 
 
 
-
-    const productos = [
-    {
-      id: 1, 
-      nombre: "Crema Antiarrugas",
-      marca: "Eucerin",
-      precio: 15000 
-    },  
-    {
-      id: 2, 
-      nombre: "Crema AntiEdad" ,
-      marca: "Dermaglos" ,precio: 7500 
-    },  
-    {  
-      id: 3,
-      nombre: "Acido Hialuronico",
-      marca: "ART FILLER",
-      precio: 30000 
-    },  
-    {
-      id: 4,
-      nombre: "Botox",
-      marca: "Dysport",
-      precio: 100000 
-    },  
-  ];
-
-  localStorage.setItem("carrito", JSON.stringify(productos));
-
-  const agregar = (id) => {
-    let producto = productos.find((item) => item.id === id);
-    console.log(producto);
-  };
-
-  productos.forEach((item) => {
-    let div = document.createElement("div");
-    div.className = "bloquemain"; 
-    div.innerHTML = `
-      <h2>ID: ${item.id}</h2>
-      <p>Nombre: ${item.nombre}</p>
-      <p>Marca: ${item.marca}</p>
-      <p>Precio: ${item.precio}</p>
-      <button id="boton${item.id}">Agregar</button> 
-    `;
-    document.body.append(div);
-
-    let boton = document.getElementById(`boton${item.id}`);
-    boton.addEventListener("click", () => agregar(item.id)); 
-  }); 
 
   
+
+
 
 
 
